c55ec709a2d8c62e38f6d892b9148387c0caf050
Kiera Reed <kierajreed@gmail.com>
2023-12-15 20:31:58 -0500

    first working draft

---
+++ public/recipe.js
@@ -0,0 +1,29 @@
+const getParam = p => new URLSearchParams(window.location.search).get(p);
+
+function renderRecipe(recipe) {
+    return `<div>
+        <div>
+            <h1>${recipe.name}</h1>
+            <h3>By ${recipe.author}</h3>
+        </div>
+        <div>${ recipe.ingredients.map(e =>
+            `${e.title!=='' ? `<h2>${e.title}</h2>` : ''}
+            <ul>
+                ${e.items.map(q => `<li>${q}</li>`).join('\n')}
+            </ul>`
+        )}</div>
+        <div>
+            <ol>${
+                recipe.preparation.map(q => `<li>${q}</li>`).join('\n')
+            }</ol>
+        </div>
+        <div>${
+            recipe.tags.map(q => `<span>${q}</span>`).join('\n')
+        }</div>
+    </div>`;
+}
+
+document.addEventListener('DOMContentLoaded', async e => {
+    let recipeRes = await fetch('recipes/'+getParam('r')+'.json');
+    document.getElementById('outer-recipe').innerHTML = renderRecipe(await recipeRes.json());
+});
